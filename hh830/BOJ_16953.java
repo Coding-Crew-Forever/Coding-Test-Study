161ddf31c4270339152e34a8664ed01f4bcf84b3
장현희 <99874673+hh830@users.noreply.github.com>
2025-06-22 21:03:27 +0900

    [BOJ] A → B / 실버2 / 30분

--- hh830/BOJ_16953.java
+++ hh830/BOJ_16953.java
@@ -1,31 +1,45 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] input = bufferedReader.readLine().split(" ");
-        long A = Long.parseLong(input[0]);
-        long B = Long.parseLong(input[1]);
-        
-        int count = 1;
-        while (B > A) {
-            if (B % 10 == 1) {
-                B /= 10;
-            } else if (B % 2 == 0) {
-                B /= 2;
-            } else {
-                break;
+        String[] tokens = bufferedReader.readLine().split(" ");
+        long A = Long.parseLong(tokens[0]);
+        long B = Long.parseLong(tokens[1]);
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(A, 1));
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            long num = node.num;
+            int count = node.count;
+
+            if (num == B) {
+                System.out.println(count);
+                return;
             }
-            count++;
+
+            if (num * 2 <= B) {
+                queue.add(new Node(num * 2, count + 1));
+            }
+
+            if (num * 10 + 1 <= B) {
+                queue.add(new Node(num * 10 + 1, count + 1));
+            }
         }
 
-        if (A == B) {
-            System.out.println(count);
-        } else {
-            System.out.println(-1);
+        System.out.println(-1);
+    }
+
+    static class Node {
+        long num;
+        int count;
+        Node(long num, int count) {
+            this.num = num;
+            this.count = count;
         }
     }
 }
+
