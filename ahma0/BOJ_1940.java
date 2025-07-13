79572a72ec35d088253c3153d3ce0039447e7167
ahma0 <dsd932@naver.com>
2025-07-13 20:49:37 +0900

    [BOJ] 주몽 / 실버4 / 15분
    https://www.acmicpc.net/problem/1940

---
+++ ahma0/BOJ_1940.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        if (n < 2) {
+            System.out.println(0);
+            return;
+        }
+
+        Arrays.sort(arr);
+
+        int cnt = 0;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (arr[i] + arr[j] == m) {
+                    cnt++;
+                    arr[j] = m + 1;
+
+                    break;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
