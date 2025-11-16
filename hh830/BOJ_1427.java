3334124f463ef6b68ddc742d49b601de2229b4d5
장현희 <99874673+hh830@users.noreply.github.com>
2025-11-16 18:11:34 +0900

    [BOJ] 소트인사이드 / 실버5 / 10분

---
+++ hh830/BOJ_1427.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class BOJ_1427 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String n = bufferedReader.readLine();
+        char arr[] = n.toCharArray();
+
+        Arrays.sort(arr);
+
+        for(int i=arr.length-1; i>=0; i--) {
+            System.out.print(arr[i]);
+        }
+    }
+}
